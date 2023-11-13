cb75f46f9104270d9ab3991d2b6fa02da286473b
agir <agirsuki@gmail.com>
2023-11-13 19:50:26 +0800

    scss自动导入

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,5 +2,14 @@
     <div>
         我是首页
         <RouterView/>
+        <div class="test">
+            ceshi
+        </div>
     </div>
 </template>
+
+<style scoped lang="scss">
+.test {
+    color: $priceColor;
+}
+</style>
