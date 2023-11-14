7f3fc73be7f0b17127b1ca67f28a43984a2c42d3
agir <agirsuki@gmail.com>
2023-11-14 15:11:37 +0800

    路由缓存问题解决

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { getCategory } from '@/apis/category';
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, onBeforeRouteUpdate } from 'vue-router'
 import { getBannerList } from '@/apis/home';
 import GoodsItem from '../Home/components/GoodsItem.vue';
 const route = useRoute()
 
 const category = ref({})
-const getCategoryData = async () => {
-    const { result } = await getCategory(route.params.id)
+const getCategoryData = async (id = route.params.id) => {
+    const { result } = await getCategory(id)
     console.log(result);
     category.value = result
 }
@@ -21,6 +21,14 @@ const getBanner = async () => {
     bannerList.value = result
 }
 onMounted(() => getBanner())
+
+// 避免组件复用导致生命周期钩子只执行一次
+// watch(
+//     () => route.params.id, 
+//     () => getCategoryData()
+// )
+// 路由发生改变 重新发送请求
+onBeforeRouteUpdate((to) => getCategoryData(to.params.id))
 </script>
 
 <template>
