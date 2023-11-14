337011a36e4bda0b58649d4e0fc7566a88818a0f
agir <agirsuki@gmail.com>
2023-11-14 09:10:14 +0800

    banner轮播图

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,5 +1,41 @@
+<script setup>
+import { getBannerList } from '@/apis/home';
+import { ref, onMounted } from 'vue'
+
+const bannerList = ref([])
+const getBanner = async () => {
+    const { result } = await getBannerList()
+    bannerList.value = result
+}
+onMounted(() => getBanner())
+</script>
+
+
+
 <template>
-    <div>
-        banner
-    </div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
